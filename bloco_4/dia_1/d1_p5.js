04546132d459af093e8ff9d66625ed5f8a480126
Breno Rodrigues <brenorm@gmail.com>
2021-02-28 19:23:14 -0300

    Exercicio Bloco 4/Dia 1/Parte V

---
+++ bloco_4/dia_1/d1_p5.js
@@ -0,0 +1,16 @@
+let situacaoCandidato = "";
+
+switch (situacaoCandidato) {
+    case "aprovada":
+        console.log("Aprovada")
+        break;
+    case "lista":
+        console.log("Lista")
+        break;
+    case "reprovada":
+        console.log("Reprovada")
+        break;
+    default:
+        console.log("Não se aplica")
+        break;
+}
