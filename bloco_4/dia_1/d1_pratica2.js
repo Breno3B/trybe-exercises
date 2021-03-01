395414186e8d28a26839077118c926b867923934
Breno Rodrigues <brenorm@gmail.com>
2021-02-28 22:55:37 -0300

    Exercicio Bloco 4/Dia 1/Pratica 2

---
+++ bloco_4/dia_1/d1_pratica2.js
@@ -0,0 +1,21 @@
+/*
+2 - Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados:
+*/
+
+let fatorA = 199;
+let fatorB = 199;
+
+
+// COMPRARAÇÃO
+if ( fatorA > fatorB ) {
+  console.log( "MAIOR número entre os dois: " + fatorA );
+  } else if ( fatorA < fatorB ) {
+    console.log( "MAIOR número entre os dois: " + fatorB );
+    } else {
+      console.log( "Valores são IGUAIS: " + "Fator A = " + fatorA + " | Fator B = " + fatorB );
+      }   
+
+
+
+
+
