4e8d355666118e634ad4365dee435a63bc24a318
Breno Rodrigues <brenorm@gmail.com>
2021-03-01 18:57:12 -0300

    Exercicio Bloco 4/Dia 4/Funçoes

---
+++ bloco_4/dia_4/d4_funcoes.js
@@ -0,0 +1,33 @@
+let num1 = 1;
+let num2 = 2;
+
+
+// ADIÇÃO
+function sum(fatorA, fatorB) {
+    return (fatorA + fatorB);    
+}
+console.log("Resultado da SOMA dos dois números: " + sum(num1, num2));
+
+// SUBTRAÇÃO
+function subtraction(fatorA, fatorB) {
+    return (fatorA - fatorB);
+}
+console.log("Resultado da SUBTRAÇÃO dos dois números: " + subtraction(num1, num2));
+
+// MULTIPLICAÇÃO
+function multiplication(fatorA, fatorB) {
+    return (fatorA * fatorB);
+}
+console.log("Resultado da MULTIPLICAÇÃO dos dois números: " + multiplication(num1, num2));
+
+// DIVISÃO
+function division(fatorA, fatorB) {
+    return (fatorA / fatorB);
+}
+console.log("Resultado da DIVISÃO dos dois números: " + division(num1, num2));
+
+// MÓDULO
+function modulo(fatorA, fatorB) {
+    return (fatorA % fatorB);
+}
+console.log("Resultado do MÓDULO da divisão entre os dois números: " + modulo(num1, num2));
