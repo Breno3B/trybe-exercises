50a09e6b0a27518f0df3db9ccfe03f6ec1616e91
Breno Rodrigues <brenorm@gmail.com>
2021-03-01 00:13:15 -0300

    Exercicio Bloco 4/Dia 1/Pratica 3: Atualizaçao

--- bloco_4/dia_1/d1_pratica3.js
+++ bloco_4/dia_1/d1_pratica3.js
@@ -2,12 +2,23 @@
 3 - Faça um programa que retorne o maior de três números. Defina no começo do programa três variáveis com os valores que serão comparados.
 */
 
-let fatorA = 5;
-let fatorB = 199;
-let fatorC = 47;
+let fatorA = 10;
+let fatorB = 15;
+let fatorC = 15;
 
 
-// COMPRARAÇÃO
-if ( (fatorA > fatorB) && fatorA > fatorC ) {
-  console.log( "MAIOR número entre os dois: " + fatorA );
+// COMPRARAÇÃO:
+// Lógica cópiada da página: https://sites.google.com/site/inflp2009/2--instrucoes-compostas/if-else/maior-de-tres-numeros
+if ( fatorA > fatorB ) {
+  if ( fatorA > fatorC ) {
+    console.log( "MAIOR número entre os trẽs é: " + fatorA );
+  } else {
+    console.log( "MAIOR número entre os trẽs é: " + fatorC );
+  }
+} else {
+  if ( fatorB > fatorC ) {
+    console.log( "MAIOR número entre os trẽs é: " + fatorB );
+  } else {
+    console.log( "MAIOR número entre os trẽs é: " + fatorC );
+  }  
 }
