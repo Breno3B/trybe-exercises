9526ed5bfb9dc244935112d27904b06e0858da8d
Breno Rodrigues <brenorm@gmail.com>
2021-04-05 22:17:23 -0300

    Bloco 8/Dia 3/Exercício 6

--- bloco_8/dia_3/exercise6.js
+++ bloco_8/dia_3/exercise6.js
@@ -72,7 +72,8 @@ const expectedResult = [
 function oldBooks() {
   // escreva seu código aqui
   // Crie um array com o nome de todos os livros com mais de 60 anos de publicação.
-  
+
+  return books.filter((book) => 2021 - book.releaseYear > 60).map((book) => book.name);
 }
 
 assert.deepStrictEqual(oldBooks(), expectedResult);
