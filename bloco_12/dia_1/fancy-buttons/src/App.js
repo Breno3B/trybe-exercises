90541d0a9fcfc3905d41990552ed4d72b5a6e366
Breno3B <brenorm@gmail.com>
2021-04-30 00:24:26 -0300

    fancy-buttons-react-app: Pra fixar

--- bloco_12/dia_1/fancy-buttons/src/App.js
+++ bloco_12/dia_1/fancy-buttons/src/App.js
@@ -7,7 +7,8 @@ class App extends React.Component {
     // A função abaixo vincula "manualmente" o `this` à nossa função
     this.handleClick = this.handleClick.bind(this)
     this.state = {
-      numeroDeCliques: 0
+      numeroDeCliques: 0,
+      backGroundColor: 'Green'
     }
   }
 
@@ -16,14 +17,25 @@ class App extends React.Component {
       numeroDeCliques: estadoAnterior.numeroDeCliques + 1
     }))
 
-    console.log('Clicou no botão!')
-    console.log(this)
+    if ((this.state.numeroDeCliques % 2) === 0) {
+      this.setState((estadoAnterior, _props) => ({
+        backGroundColor: 'White'
+      }))
+      console.log(this.state.backGroundColor)
+    } else {
+      this.setState((estadoAnterior, _props) => ({
+        backGroundColor: 'Green'
+      }))
+      console.log(this.state.backGroundColor)
+    }
+
+    // console.log(this)
   }
 
   render() {
     return (
       <div>
-        <button className='' onClick={this.handleClick}>Botão: {this.state.numeroDeCliques}</button>
+        <button className={this.state.backGroundColor} onClick={this.handleClick}>Botão: {this.state.numeroDeCliques}</button>
       </div>
     )
   }
