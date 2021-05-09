cc8230b6fdd6363317fdc55fdb6b429e31b89218
Breno3B <brenorm@gmail.com>
2021-05-08 22:00:37 -0300

    Bloco 11/Dia 2/Exercicio Fixacao: PropTypes

---
+++ bloco_11/dia_2/fixation-exercises-11-2-proptypes/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Greeting from './Greeting'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Greeting name='Breno' lastName='Rodrigues' />
+    </div>
+  )
+}
+
+export default App;
