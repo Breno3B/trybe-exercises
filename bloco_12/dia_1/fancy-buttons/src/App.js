d133d09bb5e2bcbf1f1d3809125dccc96bfdd65d
Breno3B <brenorm@gmail.com>
2021-05-10 09:50:04 -0300

    Ajuste para midança de branch

--- bloco_12/dia_1/fancy-buttons/src/App.js
+++ bloco_12/dia_1/fancy-buttons/src/App.js
@@ -2,17 +2,18 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
-  constructor() {
-    super()
+  constructor(_props) {
+    super(_props)
     // A função abaixo vincula "manualmente" o `this` à nossa função
     this.handleClick = this.handleClick.bind(this)
+
     this.state = {
       numeroDeCliques: 0,
       backGroundColor: 'Green'
     }
   }
 
-  handleClick() {
+  handleClick = () => {
     this.setState((estadoAnterior, _props) => ({
       numeroDeCliques: estadoAnterior.numeroDeCliques + 1
     }))
@@ -21,11 +22,13 @@ class App extends React.Component {
       this.setState((estadoAnterior, _props) => ({
         backGroundColor: 'White'
       }))
+
       console.log(this.state.backGroundColor)
     } else {
       this.setState((estadoAnterior, _props) => ({
         backGroundColor: 'Green'
       }))
+
       console.log(this.state.backGroundColor)
     }
 
@@ -34,8 +37,8 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
-        <button className={this.state.backGroundColor} onClick={this.handleClick}>Botão: {this.state.numeroDeCliques}</button>
+      <div className="App">
+        <button className={this.state.backGroundColor} onClick={() => this.handleClick()}>Botão: {this.state.numeroDeCliques}</button>
       </div>
     )
   }
