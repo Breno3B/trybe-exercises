f2b91100fdc582a4470d10d0f8f3c30fb284c6a1
Breno3B <brenorm@gmail.com>
2021-05-12 09:28:39 -0300

    Ajuste Identacao

--- bloco_12/dia_2/controled-components/src/CampoSelect.js
+++ bloco_12/dia_2/controled-components/src/CampoSelect.js
@@ -8,9 +8,9 @@ class CampoSelect extends Component {
             <label>
                 Campo select:
                 <select name="campoSelect" value={value} onChange={onChange}>
-                <option value="Opção 1">Opção 1</option>
-                <option value="Opção 2">Opção 2</option>
-                <option value="Opção 3">Opção 3</option>
+                  <option value="Opção 1">Opção 1</option>
+                  <option value="Opção 2">Opção 2</option>
+                  <option value="Opção 3">Opção 3</option>
                 </select>
             </label>
         )
