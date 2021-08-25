a06e9e0fe559c67088b6cffcdb0a6bbe3481afc7
Breno3B <brenorm@gmail.com>
2021-08-25 02:08:54 -0300

    Bloco 26/Dia 1/sorteio.js

---
+++ bloco_26/dia_1/my-scripts/sorteio.js
@@ -0,0 +1,28 @@
+const {questionInt, question } = require('readline-sync')
+
+function printResult(userNumber, randomNumber) {
+  if (userNumber !== randomNumber) {
+    return console.log(`Opa, não foi dessa vez. O número era ${randomNumber}`);
+  }
+  
+  return console.log('Parabéns, número correto!');
+}
+
+function game(){
+  console.log('\nJogo de Adivinhação: Acerte qual foi o número aleatório gerado!\n');
+  
+  const randomNumber = Math.round( Math.random() * 10 );
+  const userNumber = questionInt('Digite um número inteiro entre 0 e 10: ');
+
+  printResult(userNumber, randomNumber);
+
+  const restart = question('\n\nVocê deseja jogar novamente? (Digite s para sim, ou qualquer outra tecla para sair:\n ');
+
+  if (restart !== 's') {
+    return console.log('\nAté breve!');
+  }
+  
+  game();
+}
+
+game();
