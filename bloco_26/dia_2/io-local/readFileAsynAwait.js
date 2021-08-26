b985c312d7cc8af3f11566604eb13c25eeab50a5
Breno3B <brenorm@gmail.com>
2021-08-26 20:10:17 -0300

    Bloco 26/Dia 2/Exemplos conteúdo: Flag

--- bloco_26/dia_2/io-local/readFileAsynAwait.js
+++ bloco_26/dia_2/io-local/readFileAsynAwait.js
@@ -2,10 +2,11 @@ const fs = require('fs').promises;
 
 async function main() {
   try {
-    await fs.writeFile('./meu-arquivo.txt', 'Meu textão', { flag: 'w' }); //{ flag: 'wx' }
+    await fs.writeFile('./meu-arquivo.txt', 'Meu textão', { flag: 'w' }); //{ flag: 'wx' } // lança um erro caso o arquivo já exista
     console.log('Arquivo escrito com sucesso!');
   } catch (err) {
     console.error(`Erro ao escrever o arquivo: ${err.message}`);
+    console.error(err);
   }
 }
 
