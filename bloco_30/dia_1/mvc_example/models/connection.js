74264d48bd9f583f5b76a09a7a4c64944660cfdc
Breno Rodrigues <brenorm@gmail.com>
2021-10-27 17:13:47 -0300

    Criação de rota de Autores, controller de autores, model de autores, template engne de autores.

--- bloco_30/dia_1/mvc_example/models/connection.js
+++ bloco_30/dia_1/mvc_example/models/connection.js
@@ -1,9 +1,10 @@
 const mysql = require('mysql2/promise');
+require('dotenv').config();
 
 const connection = mysql.createPool({
-  host: 'localhost',
-  user: 'trybe',
-  password: 'Trybe@12345',
-  database: 'mvc_example'});
+  host: process.env.HOSTNAME,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.DB_NAME});
 
 module.exports = connection;
