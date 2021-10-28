055b7ab593ec0482b3481781174741142e602af0
Breno Rodrigues <brenorm@gmail.com>
2021-10-27 21:37:55 -0300

    Conclusão do exemplo

--- bloco_30/dia_1/mvc_example/index.js
+++ bloco_30/dia_1/mvc_example/index.js
@@ -5,10 +5,19 @@ const authorController = require('./controllers/authorController');
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
+
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
 app.get('/authors', authorController.listAuthors);
+
+app.get('/authors/new', authorController.newAuthor);
+
+app.get('/authors/:id', authorController.showAuthor);
+
+app.post('/authors', authorController.createAuthor);
 
 const PORT = process.env.PORT || 3000;
 
