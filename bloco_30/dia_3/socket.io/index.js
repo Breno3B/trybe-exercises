6a750b54883d6d5f049f65e5bbf36c4ff961af10
Breno Rodrigues <brenorm@gmail.com>
2021-11-01 15:24:39 -0300

    Criação do Listener para evento PING

--- bloco_30/dia_3/socket.io/index.js
+++ bloco_30/dia_3/socket.io/index.js
@@ -11,7 +11,11 @@ const io = require('socket.io')(http, {
 });
 
 io.on('connection', (socket) => {
-  console.log(`Usuário conectado. ID: ${socket.id} `)
+  console.log(`Usuário conectado. ID: ${socket.id} `);
+
+  socket.on('ping', () => {
+    console.log(`${socket.id} emitiu um ping!`);
+  });
 });
 
 app.get('/', (req, res) => {
